5d3f9489f1b2e34205e356f1096e766fa09f28b0
Wang Yu <79128379+kevin861222@users.noreply.github.com>
2024-01-13 00:40:28 +0800

    update main.c
    3 times testing

--- testbench/main.c
+++ testbench/main.c
@@ -21,67 +21,7 @@
 #ifdef USER_PROJ_IRQ0_EN
 #include <irq_vex.h>
 #endif
-void main()
-{
-	reg_mprj_io_31 = GPIO_MODE_MGMT_STD_OUTPUT;
-	reg_mprj_io_30 = GPIO_MODE_MGMT_STD_OUTPUT;
-	reg_mprj_io_29 = GPIO_MODE_MGMT_STD_OUTPUT;
-	reg_mprj_io_28 = GPIO_MODE_MGMT_STD_OUTPUT;
-	reg_mprj_io_27 = GPIO_MODE_MGMT_STD_OUTPUT;
-	reg_mprj_io_26 = GPIO_MODE_MGMT_STD_OUTPUT;
-	reg_mprj_io_25 = GPIO_MODE_MGMT_STD_OUTPUT;
-	reg_mprj_io_24 = GPIO_MODE_MGMT_STD_OUTPUT;
-	reg_mprj_io_23 = GPIO_MODE_MGMT_STD_OUTPUT;
-	reg_mprj_io_22 = GPIO_MODE_MGMT_STD_OUTPUT;
-	reg_mprj_io_21 = GPIO_MODE_MGMT_STD_OUTPUT;
-	reg_mprj_io_20 = GPIO_MODE_MGMT_STD_OUTPUT;
-	reg_mprj_io_19 = GPIO_MODE_MGMT_STD_OUTPUT;
-	reg_mprj_io_18 = GPIO_MODE_MGMT_STD_OUTPUT;
-	reg_mprj_io_17 = GPIO_MODE_MGMT_STD_OUTPUT;
-	reg_mprj_io_16 = GPIO_MODE_MGMT_STD_OUTPUT;
-
-	reg_mprj_io_15 = GPIO_MODE_USER_STD_OUTPUT;
-	reg_mprj_io_14 = GPIO_MODE_USER_STD_OUTPUT;
-	reg_mprj_io_13 = GPIO_MODE_USER_STD_OUTPUT;
-	reg_mprj_io_12 = GPIO_MODE_USER_STD_OUTPUT;
-	reg_mprj_io_11 = GPIO_MODE_USER_STD_OUTPUT;
-	reg_mprj_io_10 = GPIO_MODE_USER_STD_OUTPUT;
-	reg_mprj_io_9  = GPIO_MODE_USER_STD_OUTPUT;
-	reg_mprj_io_8  = GPIO_MODE_USER_STD_OUTPUT;
-	reg_mprj_io_7  = GPIO_MODE_USER_STD_OUTPUT;
-	reg_mprj_io_4  = GPIO_MODE_USER_STD_OUTPUT;
-	reg_mprj_io_3  = GPIO_MODE_USER_STD_OUTPUT;
-	reg_mprj_io_2  = GPIO_MODE_USER_STD_OUTPUT;
-	reg_mprj_io_1  = GPIO_MODE_USER_STD_OUTPUT;
-	reg_mprj_io_0  = GPIO_MODE_USER_STD_OUTPUT;
-
-	reg_mprj_io_6  = GPIO_MODE_MGMT_STD_OUTPUT;
-	reg_mprj_io_5  = GPIO_MODE_USER_STD_INPUT_NOPULL;
-
-	// Configure LA probes [31:0], [127:64] as inputs to the cpu 
-	// Configure LA probes [63:32] as outputs from the cpu
-	reg_la0_oenb = reg_la0_iena = 0x00000000;    // [31:0]
-	reg_la1_oenb = reg_la1_iena = 0x00000000;    // [63:32]
-	reg_la2_oenb = reg_la2_iena = 0x00000000;    // [95:64]
-	reg_la3_oenb = reg_la3_iena = 0x00000000;    // [127:96]
-	// UART Receive Interrupt
-#ifdef USER_PROJ_IRQ0_EN
-    int mask;
-	// unmask USER_IRQ_0_INTERRUPT
-	mask = irq_getmask();
-	mask |= 1 << USER_IRQ_0_INTERRUPT; // USER_IRQ_0_INTERRUPT = 2
-	irq_setmask(mask);
-	// enable user_irq_0_ev_enable
-	user_irq_0_ev_enable_write(1);
-#endif
-
-	// Set User Project Slaves Enable
-	reg_wb_enable = 1;
-
-	// Now, apply the configuration
-	reg_mprj_xfer = 1;
-	while (reg_mprj_xfer == 1);
-
+void /*__attribute__ ( ( section ( ".mprjram" ) ) )*/ test(){
 	// start flag - FIR
 	reg_mprj_datal = (0xAB00<<16);
 	// FIR tap
@@ -160,4 +100,70 @@ void main()
 	while(!(reg_DMA_cfg & (1<<DMA_cfg_idle))) ;
 	// end flag - qsort
 	reg_mprj_datal = (0xAB21<<16);
+
+}
+void main()
+{
+	reg_mprj_io_31 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_30 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_29 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_28 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_27 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_26 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_25 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_24 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_23 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_22 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_21 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_20 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_19 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_18 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_17 = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_16 = GPIO_MODE_MGMT_STD_OUTPUT;
+
+	reg_mprj_io_15 = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_14 = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_13 = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_12 = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_11 = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_10 = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_9  = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_8  = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_7  = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_4  = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_3  = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_2  = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_1  = GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_0  = GPIO_MODE_USER_STD_OUTPUT;
+
+	reg_mprj_io_6  = GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_5  = GPIO_MODE_USER_STD_INPUT_NOPULL;
+
+	// Configure LA probes [31:0], [127:64] as inputs to the cpu 
+	// Configure LA probes [63:32] as outputs from the cpu
+	reg_la0_oenb = reg_la0_iena = 0x00000000;    // [31:0]
+	reg_la1_oenb = reg_la1_iena = 0x00000000;    // [63:32]
+	reg_la2_oenb = reg_la2_iena = 0x00000000;    // [95:64]
+	reg_la3_oenb = reg_la3_iena = 0x00000000;    // [127:96]
+	// UART Receive Interrupt
+#ifdef USER_PROJ_IRQ0_EN
+    int mask;
+	// unmask USER_IRQ_0_INTERRUPT
+	mask = irq_getmask();
+	mask |= 1 << USER_IRQ_0_INTERRUPT; // USER_IRQ_0_INTERRUPT = 2
+	irq_setmask(mask);
+	// enable user_irq_0_ev_enable
+	user_irq_0_ev_enable_write(1);
+#endif
+
+	// Set User Project Slaves Enable
+	reg_wb_enable = 1;
+
+	// Now, apply the configuration
+	reg_mprj_xfer = 1;
+	while (reg_mprj_xfer == 1);
+
+	test();
+	test();
+	test();
 }
