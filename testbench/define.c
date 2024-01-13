32f0fae14159057ac5781eb6d40db8a945997e39
Wang Yu <79128379+kevin861222@users.noreply.github.com>
2024-01-13 17:04:43 +0800

    Update define.c ( B_T )

--- testbench/define.c
+++ testbench/define.c
@@ -22,12 +22,13 @@ int mat_A[NUM_MAT_A] =
         0, 1, 2, 3,
         0, 1, 2, 3,
 };
-int mat_B[NUM_MAT_B] = 
+// Transpose matrix
+int mat_B_T[NUM_MAT_B] = 
 {   
-     1,  2,  3,  4,
-     5,  6,  7,  8,
-     9, 10, 11, 12,
-    13, 14, 15, 16,
+     1,  5,  9, 13,
+     2,  6, 10, 14,
+     3,  7, 11, 15,
+     4,  8, 12, 16,
 };
 int mat_output[NUM_MAT_OUTPUT];
 
